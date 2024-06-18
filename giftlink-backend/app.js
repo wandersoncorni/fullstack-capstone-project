c3acae5298c1bbb29d0624c6de14fce46eb63750
wandersoncorni <wandersoncorni@gmail.com>
2024-06-18 10:18:08 -0300

    Module 2 - Search routes implementation

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -22,11 +22,10 @@ app.use(express.json());
 
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-const giftRoutes = require('./routes/.giftRoutes');
+const giftRoutes = require('./routes/giftRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
-
+const searchRoutes = require('./routes/searchRoutes');
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -38,7 +37,7 @@ app.use(pinoHttp({ logger }));
 app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/search', searchRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
